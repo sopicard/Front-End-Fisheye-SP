8574e5f3c42dfe1eb87cb98cceb08b432593541f
Sophie Picard <sogautrelet@gmail.com>
2023-03-12 21:00:47 +0100

    integration of index page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,41 @@
+// Crée des objets de photographe
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    // Découpe les données des photographes pour obtenir les propriétés choisies
+    const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    // Chemin d'accès à l'img du photographe
+    const picture = `assets/photographers/photographersProfile/${portrait}`;
 
+    // Crée la carte pour le photographe
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+
+        // Crée les éléments HTML pour chaque carte
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        const h2 = document.createElement("h2");
+        const location = document.createElement("p1");
+        const taglineElement = document.createElement("p2");
+        const priceElement = document.createElement("p3");
+
+        // Configure les propriétés des éléments HTML
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        location.textContent = `${city}, ${country}`;
+        taglineElement.textContent = tagline;
+        priceElement.textContent =  `${price}€/jour`;
+
+        // Ajoute les éléments HTML à l'élément "article"
         article.appendChild(img);
         article.appendChild(h2);
-        return (article);
+        article.appendChild(location);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
+        
+        // Retourne l'élément "article" avec la carte
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    // Retourne un objet contenant les propriétés du photographe
+    //  et la fonction qui crée la carte du photographe
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,33 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+// Récupère les données des photographes à partir du fichier JSON
+async function getPhotographers() {
+    const response = await fetch('../../data/photographers.json');
+    const data = await response.json();
+    const photographers = data.photographers;
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    // Retourne le tableau des photographes
+    return photographers;
+}
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
+//Affiche la carte de chaque photographe
+async function displayData() {
+    // Attend que la promesse retournée par getPhotographers soit résolue
+    const photographers = await getPhotographers();
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-    
-    init();
+    const photographersSection = document.querySelector(".photographer_section");
+
+    // Crée une carte pour chaque photographe
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
+
+// Initialise l'application en affichant les données des photographes
+async function init() {
+    await displayData();
+}
+
+// Appelle la fonction init pour démarrer l'application
+init();
     
